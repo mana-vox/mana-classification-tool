38dd57597bbd447a9a0d228e2c95e76889c5b32c
lulupet <lucas.petit@student.ecp.fr>
2021-04-09 11:48:33 +0200

    feat(sentences): automatically create the sentences when adding a new article

--- api/src/article/tests.py
+++ api/src/article/tests.py
@@ -1,7 +1,7 @@
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from .models import Article
+from .models import Article, Sentence
 from account.models import Account
 
 
@@ -10,7 +10,7 @@ APP_NAME = '/article/'
 
 class ArticleTests(APITestCase):
 
-    def test_new_article(self):
+    def test_new_article_1(self):
         data_register = {
             'email': 'nonexisting@manavox.com',
             'password': 'passwordtest'
@@ -28,3 +28,114 @@ class ArticleTests(APITestCase):
         response_2 = self.client.post(APP_NAME + 'new', data_article, format='json')
         self.assertEqual(response_2.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Article.objects.count(), 1)
+        self.assertEqual(Sentence.objects.count(), 1)
+        self.assertEqual(Sentence.objects.first().sentence_text, 'some article content')
+
+    def test_new_article_2(self):
+        data_register = {
+            'email': 'nonexisting@manavox.com',
+            'password': 'passwordtest'
+        }
+        response_1 = self.client.post('/account/' + 'register', data_register, format='json')
+        account = Account.objects.get(email='nonexisting@manavox.com')
+        account.is_staff = True
+        account.save()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + response_1.data['token'])
+        data_article = {
+            'id_article': 1,
+            'full_text': 'One sentence.',
+            'url': 'https://www.wikipedia.org/'
+        }
+        response_2 = self.client.post(APP_NAME + 'new', data_article, format='json')
+        self.assertEqual(response_2.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Article.objects.count(), 1)
+        self.assertEqual(Sentence.objects.count(), 1)
+        self.assertEqual(Sentence.objects.first().sentence_text, 'One sentence')
+
+    def test_new_article_3(self):
+        data_register = {
+            'email': 'nonexisting@manavox.com',
+            'password': 'passwordtest'
+        }
+        response_1 = self.client.post('/account/' + 'register', data_register, format='json')
+        account = Account.objects.get(email='nonexisting@manavox.com')
+        account.is_staff = True
+        account.save()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + response_1.data['token'])
+        data_article = {
+            'id_article': 1,
+            'full_text': 'One sentence. Another sentence',
+            'url': 'https://www.wikipedia.org/'
+        }
+        response_2 = self.client.post(APP_NAME + 'new', data_article, format='json')
+        self.assertEqual(response_2.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Article.objects.count(), 1)
+        self.assertEqual(Sentence.objects.count(), 2)
+        self.assertEqual(Sentence.objects.first().sentence_text, 'One sentence')
+        self.assertEqual(Sentence.objects.all()[1].sentence_text, 'Another sentence')
+
+    def test_new_article_4(self):
+        data_register = {
+            'email': 'nonexisting@manavox.com',
+            'password': 'passwordtest'
+        }
+        response_1 = self.client.post('/account/' + 'register', data_register, format='json')
+        account = Account.objects.get(email='nonexisting@manavox.com')
+        account.is_staff = True
+        account.save()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + response_1.data['token'])
+        data_article = {
+            'id_article': 1,
+            'full_text': 'One sentence! Another sentence',
+            'url': 'https://www.wikipedia.org/'
+        }
+        response_2 = self.client.post(APP_NAME + 'new', data_article, format='json')
+        self.assertEqual(response_2.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Article.objects.count(), 1)
+        self.assertEqual(Sentence.objects.count(), 2)
+        self.assertEqual(Sentence.objects.first().sentence_text, 'One sentence')
+        self.assertEqual(Sentence.objects.all()[1].sentence_text, 'Another sentence')
+
+    def test_new_article_5(self):
+        data_register = {
+            'email': 'nonexisting@manavox.com',
+            'password': 'passwordtest'
+        }
+        response_1 = self.client.post('/account/' + 'register', data_register, format='json')
+        account = Account.objects.get(email='nonexisting@manavox.com')
+        account.is_staff = True
+        account.save()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + response_1.data['token'])
+        data_article = {
+            'id_article': 1,
+            'full_text': 'One sentence? Another sentence',
+            'url': 'https://www.wikipedia.org/'
+        }
+        response_2 = self.client.post(APP_NAME + 'new', data_article, format='json')
+        self.assertEqual(response_2.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Article.objects.count(), 1)
+        self.assertEqual(Sentence.objects.count(), 2)
+        self.assertEqual(Sentence.objects.first().sentence_text, 'One sentence')
+        self.assertEqual(Sentence.objects.all()[1].sentence_text, 'Another sentence')
+
+    def test_new_article_6(self):
+        data_register = {
+            'email': 'nonexisting@manavox.com',
+            'password': 'passwordtest'
+        }
+        response_1 = self.client.post('/account/' + 'register', data_register, format='json')
+        account = Account.objects.get(email='nonexisting@manavox.com')
+        account.is_staff = True
+        account.save()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + response_1.data['token'])
+        data_article = {
+            'id_article': 1,
+            'full_text': 'One sentence... Another sentence',
+            'url': 'https://www.wikipedia.org/'
+        }
+        response_2 = self.client.post(APP_NAME + 'new', data_article, format='json')
+        self.assertEqual(response_2.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Article.objects.count(), 1)
+        self.assertEqual(Sentence.objects.count(), 2)
+        self.assertEqual(Sentence.objects.first().sentence_text, 'One sentence')
+        self.assertEqual(Sentence.objects.all()[1].sentence_text, 'Another sentence')
--- api/src/article/views.py
+++ api/src/article/views.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 
-from .models import Article
+from .models import Article, Sentence
 
 
 @api_view(['POST'])
@@ -13,6 +13,24 @@ def new_article_view(request):
     article = Article(id_article=request.data['id_article'],
                       full_text=request.data['full_text'], url=request.data['url'])
     article.save()
+    list_sentences = request.data['full_text'].split('.')
+    list_sentences_exclamation = []
+    for sentence in list_sentences:
+        sub_list_sentences = sentence.split('!')
+        for sub_sentence in sub_list_sentences:
+            list_sentences_exclamation.append(sub_sentence)
+    list_sentences_interrogation = []
+    for sentence in list_sentences_exclamation:
+        sub_list_sentences = sentence.split('?')
+        for sub_sentence in sub_list_sentences:
+            list_sentences_interrogation.append(sub_sentence)
+    final_lists = []
+    for sentence in list_sentences_interrogation:
+        final_lists.append(sentence.lstrip().rstrip())
+    for sentence in final_lists:
+        if sentence != '':
+            sentence_object = Sentence(article=article, sentence_text=sentence)
+            sentence_object.save()
     data = {'article': 'Successfully created a new article'}
     status_code = status.HTTP_201_CREATED
     return Response(status=status_code, data=data)

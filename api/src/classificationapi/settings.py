34fd2423b5cb6e769db2e951963e1f3d23fe93f0
Lucas Petit <lucas.petit@riminder.net>
2021-05-13 13:51:41 +0200

    feat(settings): added credentials for the postgresql database

--- api/src/classificationapi/settings.py
+++ api/src/classificationapi/settings.py
@@ -95,8 +95,12 @@ WSGI_APPLICATION = 'classificationapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env.str('DATABASE_NAME'),
+        'USER': env.str('DATABASE_USER'),
+        'PASSWORD': env.str('DATABASE_password'),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
